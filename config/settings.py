7d0efd40090cad6ddad0e3f474fe39e2eec99d55
root <root@ip-172-31-21-225.eu-north-1.compute.internal>
2023-11-07 11:23:26 +0000

    changes in settings.py
    - Old local datanbase is commented
    - Database for docker db is uncommented

--- config/settings.py
+++ config/settings.py
@@ -93,26 +93,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env('POSTGRES_DB'),
-#         'USER': env('POSTGRES_USER'),
-#         'PASSWORD': env('POSTGRES_PASSWORD'),
-#         'HOST': 'metsenat-db',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'metsenat_dev_db',
-        'USER': 'metsenat_dev_user',
-        'PASSWORD': 'metsenat_dev_root',
-        'HOST': 'localhost',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': 'metsenat-db',
         'PORT': '5432',
     }
 }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'metsenat_dev_db',
+#        'USER': 'metsenat_dev_user',
+#        'PASSWORD': 'metsenat_dev_root',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 
 
 # Password validation
